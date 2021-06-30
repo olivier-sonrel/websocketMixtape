be6c7c96bbeda6daff4a4ea750f698a9e16844ed
illiesse <58207793+illiesse@users.noreply.github.com>
2021-06-30 09:28:52 +0200

    test

---
+++ public/index.js
@@ -0,0 +1,97 @@
+// const express = require('express')
+// const app = express()
+
+// app.get('/', (req,res) => {
+//   res.send('Hello World')
+// })
+
+// app.get('/health',(req,res) => {
+//   res.send('I')
+// })
+
+// app.listen(8080,() => {
+//   console.log("Server up and running")
+// })
+
+      /* marche mais pas socket
+         // var express = require('express');
+         // var app = express();
+         // const http = require('http').Server(app);
+         // http.listen(8080);
+         // const io = require('socket.io')(http);
+      fin marche mais pas socket */
+
+// const bodyParser = require('body-parser')
+
+// const server = require('http').Server(app)
+      // var server = app.listen(8080);
+      // var http = require('http');
+      // var server = http.createServer(app);
+    
+      // server.listen(8080);
+      // var io = require('socket.io')(server);
+      
+   //  var io = require('socket.io')(server);
+   //  io.listen(8080);
+
+// // ajout de socket.io
+
+// var server = app.listen(8810)
+// var io = require('socket.io').listen(server);
+
+var express = require('express');
+var app = express()
+  , http = require('http')
+  , server = http.createServer(app)
+  , io = require('socket.io')(server);
+
+// server.listen(8080);
+
+
+// app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.static('public'))
+app.get('/', function (req, res) {
+   res.sendFile('./index.html', { root: __dirname })
+})
+
+app.get('/json', function (req, res) {
+   res.status(200).json({"message":"ok"})
+})
+
+// var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
+
+// const i2c = require('i2c-bus');
+
+const ADS7830 = 0x4b;
+const CHANNELS = [0x84, 0xc4, 0x94, 0xd4, 0xa4, 0xe4, 0xb4, 0xf4];
+
+// établissement de la connexion
+io.on('connection', function (socket) {
+            const i2c1 = i2c.openSync(1);
+            var dataX=1;
+            // var dataY = 1;
+         
+            setInterval(() => {       
+              dataX = (i2c1.readWordSync(ADS7830, CHANNELS[0]) - 5911) / 30;
+            //   dataY = (i2c1.readWordSync(ADS7830, CHANNELS[1]) - 5911) / 60;
+              console.log('data X', dataX);
+            //   console.log('data Y', dataY);
+            
+            //   var obj = {dataX: dataX, dataY: dataY};
+              socket.emit('Curl', dataX);
+            }, 500);
+         
+            i2c1.closeSync();
+   // socket.emit('allo', { hello: 'world' });
+   // console.log(`Connecté au client ${socket.id}`);
+   // // socket.on('action', function () {
+   // //    console.log('A user disconnected');
+      // var xxx = 3000;
+      // socket.emit('Curl', xxx);
+})
+
+// on change app par server
+server.listen(8080, '0.0.0.0' ,function () {
+ console.log('Votre app est disponible sur localhost:8080 !')
+})
---
+++ public/public/scripts/app.js
@@ -0,0 +1,219 @@
+// const appContents = document.querySelector('.app-contents');
+// const startMessage = document.querySelector('.start-message');
+
+// var potX;
+// var potY;
+
+// let isAppInit = false;
+// appContents.style.display = 'none';
+
+// //window.addEventListener('keydown', init);
+// //window.addEventListener('click', init);
+
+// window.onload = init;
+
+// function init() {
+//   if (isAppInit) {
+//     console.log("is app init");
+//     return;
+//   }
+
+//   console.log("in init");
+//   appContents.style.display = 'block';
+//   document.body.removeChild(startMessage);
+
+//   // create web audio api context
+//   const AudioContext = window.AudioContext || window.webkitAudioContext;
+//   const audioCtx = new AudioContext();
+
+//   // create Oscillator and gain node
+//   const oscillator = audioCtx.createOscillator();
+//   const gainNode = audioCtx.createGain();
+
+//   // connect oscillator to gain node to speakers
+//   oscillator.connect(gainNode);
+//   gainNode.connect(audioCtx.destination);
+
+//   // create initial theremin frequency and volume values
+//   const WIDTH = window.innerWidth;
+//   const HEIGHT = window.innerHeight;
+
+//   const maxFreq = 6000;
+//   const maxVol = 0.02;
+//   const initialVol = 0.001;
+
+//   // set options for the oscillator
+//   oscillator.detune.value = 100; // value in cents
+//   oscillator.start(0);
+
+//   oscillator.onended = function() {
+//     console.log('Your tone has now stopped playing!');
+//   };
+
+//   gainNode.gain.value = initialVol;
+//   gainNode.gain.minValue = initialVol;
+//   gainNode.gain.maxValue = initialVol;
+
+//   // Mouse pointer coordinates
+//   var CurX;
+//   var CurY;
+
+//   // Get new mouse pointer coordinates when mouse is moved
+//   // then set new gain and pitch values
+//   document.onmousemove = updatePage;
+
+//   function updatePage(e) {
+//       KeyFlag = false;
+
+//       //CurX = potX;
+//       //CurY = potY;
+//       //TODO CHANGE VALUE X Y HERE
+//       CurX = e.pageX;
+//       CurY = e.pageY;
+//       console.log('e, curx, cury: ', e, CurX, CurY);
+//       //console.log("PotX PotY", potX, potY);
+
+//       oscillator.frequency.value = (CurX/WIDTH) * maxFreq;
+//       gainNode.gain.value = (CurY/HEIGHT) * maxVol;
+
+//       canvasDraw();
+//   }
+
+//   // mute button
+//   const mute = document.querySelector('.mute');
+
+//   mute.onclick = function() {
+//     if (mute.getAttribute('data-muted') === 'false') {
+//       gainNode.disconnect(audioCtx.destination);
+//       mute.setAttribute('data-muted', 'true');
+//       mute.innerHTML = "Unmute";
+//     } else {
+//       gainNode.connect(audioCtx.destination);
+//       mute.setAttribute('data-muted', 'false');
+//       mute.innerHTML = "Mute";
+//     };
+//   }
+
+//   // canvas visualization
+//   function random(number1,number2) {
+//     return number1 + (Math.floor(Math.random() * (number2 - number1)) + 1);
+//   }
+
+//   const canvas = document.querySelector('.canvas');
+//   const canvasCtx = canvas.getContext('2d');
+
+//   canvas.width = WIDTH;
+//   canvas.height = HEIGHT;
+
+//   function canvasDraw() {
+//     if (KeyFlag) {
+//       rX = KeyX;
+//       rY = KeyY;
+//     } else {
+//       rX = CurX;
+//       rY = CurY;
+//     }
+
+//     rC = Math.floor((gainNode.gain.value/maxVol)*30);
+
+//     canvasCtx.globalAlpha = 0.2;
+
+//     for (let i = 1; i <= 15; i = i+2) {
+//       canvasCtx.beginPath();
+//       canvasCtx.fillStyle = 'rgb(' + 100+(i*10) + ',' + Math.floor((gainNode.gain.value/maxVol)*255) + ',' + Math.floor((oscillator.frequency.value/maxFreq)*255) + ')';
+//       canvasCtx.arc(rX+random(0,50),rY+random(0,50),rC/2+i,(Math.PI/180)*0,(Math.PI/180)*360,false);
+//       canvasCtx.fill();
+//       canvasCtx.closePath();
+//     }
+//   }
+
+//   // clear screen
+//   const clear = document.querySelector('.clear');
+
+//   clear.onclick = function() {
+//     canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
+//   }
+
+//   // keyboard controls
+//   const body = document.querySelector('body');
+
+//   let KeyX = 1;
+//   let KeyY = 0.01;
+//   let KeyFlag = false;
+
+//   const ARROW_LEFT = 'ArrowLeft';
+//   const ARROW_RIGHT = 'ArrowRight';
+//   const ARROW_UP = 'ArrowUp';
+//   const ARROW_DOWN = 'ArrowDown';
+
+//   body.onkeydown = function(e) {
+//     KeyFlag = true;
+
+//     if (e.code === ARROW_LEFT) {
+//       KeyX -= 20;
+//     }
+
+//     if (e.code === ARROW_RIGHT) {
+//       KeyX += 20;
+//     }
+
+//     if (e.code === ARROW_UP) {
+//       KeyY -= 20;
+//     }
+
+//     if (e.code === ARROW_DOWN) {
+//       KeyY += 20;
+//     }
+
+//     // set max and min constraints for KeyX and KeyY
+//     if (KeyX < 1) {
+//       KeyX = 1;
+//     }
+
+//     if (KeyX > WIDTH) {
+//       KeyX = WIDTH;
+//     }
+
+//     if (KeyY < 0.01) {
+//       KeyY = 0.01;
+//     }
+
+//     if (KeyY > HEIGHT) {
+//       KeyY = HEIGHT;
+//     }
+
+//     oscillator.frequency.value = (KeyX/WIDTH) * maxFreq;
+//     gainNode.gain.value = (KeyY/HEIGHT) * maxVol;
+
+//     canvasDraw();
+//   };
+
+//   isAppInit = true;
+// }
+
+var socket = io.connect('http://localhost:8080');
+// socket.on('allo', function(datax) { //get button status from client*
+//   console.log("allo", datax);
+// });
+
+socket.on('Curl', function(data) { //get button status from client*
+    console.log("ee", data);
+    uu(data);
+  // potX = data.dataX;
+  // potY = data.dataY;
+  //console.log('dataX', data.dataX);
+  //console.log('dataY', data.dataY);
+});
+
+var uu = function(data) {
+    let audioContext = new (window.AudioContext || window.webkitAudioContext)();
+    let sourceNode = audioContext.createOscillator();
+    sourceNode.type = 'sqare'; //"sine", "square", "sawtooth", "triangle" and "custom"
+    sourceNode.frequency.value = data;
+    sourceNode.detune.value = 10080; // Ses valeur sont comprises entre -1200 et 1200.
+    //Connect the source to the speakers
+    sourceNode.connect(audioContext.destination);
+    //Make the sound audible for 100 ms
+    sourceNode.start();
+    window.setTimeout(function() { sourceNode.stop(); }, 1000);    
+}
