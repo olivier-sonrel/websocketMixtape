eff8b5878c9af506890f700d823bcfd6eb386fdb
illiesse <58207793+illiesse@users.noreply.github.com>
2021-06-30 16:04:06 +0200

    zerty

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -205,6 +205,7 @@ socket.on('Curl', function(data) { //get button status from client*
     }
     if(trig) {
       if(audioContext) {
+        console.log("tt");
         sourceNode.stop();
       }
       else {
@@ -222,7 +223,7 @@ socket.on('Curl', function(data) { //get button status from client*
       // sourceNode.connect(gainNode);
       // gainNode.connect(audioContext.destination);
       sourceNode.start();
-      window.setTimeout(function() { sourceNode.stop(); trig = false; }, 5000);
+      window.setTimeout(function() { sourceNode.stop(); trig = true; }, 1000);
       // if (mute.getAttribute('data-muted') === 'false') {
       //   //sourceNode.stop();
       //   gainNode.gain.setValueAtTime(0, audioContext.currentTime);
