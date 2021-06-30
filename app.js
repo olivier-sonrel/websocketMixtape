4b52d84fb7dd5edd680b42b15ab542e4c2927042
olivier-sonrel <o.sonrel@it-students.fr>
2021-06-30 12:51:17 +0200

    zertu

--- app.js
+++ app.js
@@ -1,59 +1,42 @@
 const express = require("express");
+const cors = require('cors');
 const http = require("http");
 const socketIo = require("socket.io");
 
-const port = process.env.PORT || 8080;
+//const port = process.env.PORT || 4001;
+const port = 4001;
 const index = require("./routes/index");
 
 const app = express();
-app.use(index);
+app.use((req,res, next)=>{
+  res.setHeader('Access-Control-Allow-Origin',"*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 const server = http.createServer(app);
 
 const io = socketIo(server);
 
-var Gpio = require('onoff').Gpio;
-const i2c = require('i2c-bus');
+let interval;
 
-const ADS7830 = 0x4b;
-const CHANNELS = [0x84, 0xc4, 0x94, 0xd4, 0xa4, 0xe4, 0xb4, 0xf4];
-
-// let interval;
-
-// io.on("connection", (socket) => {
-//   console.log("New client connected");
-//   if (interval) {
-//     clearInterval(interval);
-//   }
-//   interval = setInterval(() => getApiAndEmit(socket), 1000);
-//   socket.on("disconnect", () => {
-//     console.log("Client disconnected");
-//     clearInterval(interval);
-//   });
-// });
-
-io.on('connection', (socket) => {
-  const i2c1 = i2c.openSync(1);
-  var dataX = 1;
-  var dataY = 1;
-
-  setInterval(() => {       
-    dataX = (i2c1.readWordSync(ADS7830, CHANNELS[0]) - 5911) / 30;
-    dataY = (i2c1.readWordSync(ADS7830, CHANNELS[1]) - 5911) / 60;
-    console.log('data X', dataX);
-    console.log('data Y', dataY);
-    
-    var obj = {dataX: dataX, dataY: dataY};
-    socket.emit('Curl', obj);
-  }, 500);
-
-  i2c1.closeSync();
+io.on("connection", (socket) => {
+  console.log("New client connected");
+  if (interval) {
+    clearInterval(interval);
+  }
+  interval = setInterval(() => getApiAndEmit(socket), 1000);
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+    clearInterval(interval);
+  });
 });
 
-// const getApiAndEmit = socket => {
-//   const response = new Date();
-//   // Emitting a new message. Will be consumed by the client
-//   socket.emit("FromAPI", response);
-// };
+const getApiAndEmit = socket => {
+  const response = new Date();
+  console.log(response);
+  // Emitting a new message. Will be consumed by the client
+  socket.emit("FromAPI", response);
+};
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
