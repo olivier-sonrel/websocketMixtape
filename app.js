2c3184f384e7a1de26b4ea8d9b1432a889abda24
olivier-sonrel <o.sonrel@it-students.fr>
2021-07-07 14:19:28 +0200

    file registration and list in folder

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 //----serveur-const---
 const express = require("express");
+const formidable = require('formidable');
 const cors = require('cors');
 const http = require("http");
-const socketIo = require("socket.io");
 const port = process.env.PORT || 4001;
 const index = require("./routes/index");
-//const fs = require('fs'); //require filesystem module
 const app = express();
 
 //----electric-const---
+/*const socketIo = require("socket.io");
 const Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
 const LED = new Gpio(26, 'out'); //use GPIO pin 4 as output
 const pushButton = new Gpio(17, 'in', 'both'); //use GPIO pin 17 as input, and 'both' button presses, and releases should be handled
 
 const i2c = require('i2c-bus');
 const ADS7830 = 0x4b;
-const CHANNELS = [0x84, 0xc4, 0x94, 0xd4, 0xa4, 0xe4, 0xb4, 0xf4];
+const CHANNELS = [0x84, 0xc4, 0x94, 0xd4, 0xa4, 0xe4, 0xb4, 0xf4];*/
 
 app.use(cors());
 
@@ -27,7 +27,7 @@ app.use((req,res, next)=>{
 
 const server = http.createServer(app);
 
-const io = socketIo(server, {
+/*const io = socketIo(server, {
   cors:{
     origins: ["*"],
 
@@ -46,8 +46,8 @@ let interval;
 
 io.on("connection", (socket) => {
   //const i2c1 = i2c.openSync(1);
-/*  let dataX = 1;
-  let dataY = 1;*/
+/!*  let dataX = 1;
+  let dataY = 1;*!/
   let lightvalue = 0; //static variable for current status
   console.log("New client connected");
   if (interval) {
@@ -71,6 +71,48 @@ const getApiAndEmit = socket => {
   console.log(data);
   // Emitting a new message. Will be consumed by the client
   socket.emit("FromAPI", data);
-};
+};*/
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
+
+//upload server
+const httpFile = require("http");
+const fs = require('fs'); //require filesystem module
+const path = require('path');
+const directoryPath = path.join(__dirname, './public/samples');
+
+httpFile.createServer(function (req, res) {
+  if (req.url == '/filesList') {
+    fs.readdir(directoryPath, function (err, files) {
+      //handling error
+      if (err) {
+        return res.write('Unable to scan directory: ' + err);
+        res.end();
+      }
+      //listing all files using forEach
+      files.forEach(function (file) {
+        // Do whatever you want to do with the file
+        res.write(file);
+        res.end();
+      });
+    });
+  } else if (req.url == '/fileupload') {
+    const form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+      const oldpath = files.filetoupload.path;
+      const newpath = './public/samples/' + files.filetoupload.name;
+      fs.rename(oldpath, newpath, function (err) {
+        if (err) throw err;
+        res.write('File uploaded and moved!');
+        res.end();
+      });
+    });
+  } else {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+    res.write('<input type="file" name="filetoupload"><br>');
+    res.write('<input type="submit">');
+    res.write('</form>');
+    return res.end();
+  }
+}).listen(5000);
